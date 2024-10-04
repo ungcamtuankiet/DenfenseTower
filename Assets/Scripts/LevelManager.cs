56c36488302c9419e2f594e3e9637b3ead8fd7b8
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2024-10-04 16:24:23 +0700

    init

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject title;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateLevel()
+    {
+
+    }
+}
